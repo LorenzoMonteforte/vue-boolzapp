d1781caed16ed5cf0d7d10ae1ac6639db96c925b
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-21 17:52:48 +0100

    Contenuto layout contacts

--- index.js
+++ index.js
@@ -2,10 +2,51 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-    
+            contacts : [
+                {
+                    srcProfilePicture: "img/avatar_1.jpg",
+                    alt : "Contatto 1",
+                    name : "Michele"
+                },
+                {
+                    srcProfilePicture: "img/avatar_2.jpg",
+                    alt : "Contatto 2",
+                    name : "Fabio"
+                },
+                {
+                    srcProfilePicture: "img/avatar_3.jpg",
+                    alt : "Contatto 3",
+                    name : "Samuele"
+                },
+                {
+                    srcProfilePicture: "img/avatar_4.jpg",
+                    alt : "Contatto 4",
+                    name : "Alessandro B."
+                },
+                {
+                    srcProfilePicture: "img/avatar_5.jpg",
+                    alt : "Contatto 5",
+                    name : "Alessandro L."
+                },
+                {
+                    srcProfilePicture: "img/avatar_6.jpg",
+                    alt : "Contatto 6",
+                    name : "Claudia"
+                },
+                {
+                    srcProfilePicture: "img/avatar_7.jpg",
+                    alt : "Contatto 7",
+                    name : "Federico"
+                },
+                {
+                    srcProfilePicture : "img/avatar_8.jpg",
+                    alt : "Contatto 8",
+                    name : "Davide"
+                }
+            ]
         }
     },
     methods : {
-        
+
     }
 }).mount('#app')
